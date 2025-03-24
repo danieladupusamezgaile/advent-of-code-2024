1d9d8f1ac6b2085bbf484a40a35ca8ff424bcc0f
Daniela Dupuša-Mežgaile <danieladupusa-mezgaile@Danielas-MacBook-Air.local>
2025-03-24 14:49:10 +0200

    finished part one of day1

---
+++ day1/day1.py
@@ -0,0 +1,49 @@
+import os
+# Maybe the lists are only off by a small amount!
+# To find out, pair up the numbers and measure how far
+# apart they are. Pair up the smallest number in the left
+# list with the smallest number in the right list, then
+# the second-smallest left number with the second-smallest
+# right number, and so on.
+# Within each pair, figure out how far apart the two
+# numbers are; you'll need to add up all of those
+# distances. For example, if you pair up a 3 from the
+# left list with a 7 from the right list, the distance
+# apart is 4; if you pair up a 9 with a 3, the distance
+# apart is 6.
+
+# read the file and return the numbers in two lists
+def readListsFromFile(filename):
+    leftList = []
+    rightList = []
+
+    with open(filename, 'r') as file:
+        for line in file:
+            # split line in left and right numbers
+            left, right = map(int, line.strip().split())
+            leftList.append(left)
+            rightList.append(right)
+    
+    return leftList, rightList
+
+# calculate total distance of both lists
+def totalDistance(leftList, rightList):
+    # sort both lists
+    leftSorted = sorted(leftList)
+    rightSorted = sorted(rightList)
+
+    totalDistance = 0
+    # loop through each pair and add up total distance
+    for i in range(len(leftSorted)):
+        leftNum = leftSorted[i]
+        rightNum = rightSorted[i]
+        distance = abs(leftNum-rightNum)
+        totalDistance += distance
+    
+    return totalDistance
+
+if __name__ == "__main__":
+    filename = os.path.join(os.path.dirname(__file__), "lists.txt")
+    leftList, rightList = readListsFromFile(filename)
+    totalDistance = totalDistance(leftList, rightList)
+    print(f"Total distance: {totalDistance}")
